64cf35924e3dc7828b12481c47b8d6aa9dcf0c10
jgiani <jgiani@sdd.com.ar>
2017-12-20 20:57:36 -0300

    Correciones TODO

--- src/main/java/gaf/controller/CommonController.java
+++ src/main/java/gaf/controller/CommonController.java
@@ -9,12 +9,16 @@ import gaf.service.EstadoService;
 import gaf.service.TalleService;
 import gaf.service.TallerService;
 import gaf.util.Estados;
+import org.primefaces.context.RequestContext;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @ViewScoped
@@ -99,4 +103,31 @@ public class CommonController {
     }
     public Taller getTallerById(Integer id) {return tallerService.findById(id);}
     public Talle getTalleById(Integer id) {return talleService.findById(id);}
+
+    public void showCalendar() {
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+
+        requestContext.execute("PF('gaf-calendar-dialog').show()");
+    }
+
+    private static final String QUOTE = "'";
+    private static final String TIME_FORMAT = "T00:00:00";
+    private static final String YEAR_MONTH_DAY_FORMAT = "yyyy-MM-dd";
+
+    /* SE LLAMA DESDE EL JS EN default.xhtml:73 */
+    public String[] getFechasVencimientos() {
+        SimpleDateFormat sdf = new SimpleDateFormat(YEAR_MONTH_DAY_FORMAT);
+        List<String> result = new ArrayList<>();
+
+        //TODO - Recuperar las DueDate de los cortes e insertalos en la lista para marcarlos en el calendario
+
+        result.add(QUOTE + sdf.format(new Date()) + TIME_FORMAT + QUOTE);
+        try {
+        result.add(QUOTE + sdf.format(sdf.parse("2018-01-01")) + TIME_FORMAT + QUOTE);
+        result.add(QUOTE + sdf.format(sdf.parse("2017-12-25")) + TIME_FORMAT + QUOTE);
+        } catch (ParseException e) {}
+
+
+        return result.toArray(new String[3]);
+    }
 }
